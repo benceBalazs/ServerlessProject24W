8d4a5395cb7c1bd5738791ae994898c17a307785
HafiTheCat <f2hafner@edu.aau.at>
2025-02-18 13:46:48 +0100

    included input_handler python packages in devenv

--- devenv.nix
+++ devenv.nix
@@ -5,15 +5,39 @@
 
 { pkgs, lib, config, inputs, ... }:
 
+let
+  # define additional packages not available in Nixpkgs here
+  customPackages = [
+    # build functions_framework package
+    (pkgs.python312Packages.buildPythonPackage rec {
+      pname = "functions_framework";
+      version = "3.8.2";
+      src = pkgs.fetchPypi {
+        pname = "functions_framework";
+        version = "3.8.2";
+        sha256 = "109bcdca01244067052a605536b44d042903b3805d093cd32e343ba5affffc90";
+      };
+    })
+  ];
+in
 {
+  # automatically install teraform
   env.TENV_AUTO_INSTALL = true;
 
-  packages = with pkgs; [
+  # define packages available in Nixpkgs here
+  packages = with pkgs; customPackages ++ [
       git
-      tenv
+      tenv                                   # terraform
+      # general python
+      python312Packages.pip                  # python package manager
+      python312Packages.google-cloud-storage # google cloud storage
+      python312Packages.google-cloud-pubsub  # google cloud pubsub
   ];
 
-  languages.python.enable = true;
+  languages.python = {
+    enable = true;
+    package = pkgs.python312;
+  };
 
   enterShell = ''
   '';
