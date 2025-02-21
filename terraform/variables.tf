b526504c66d1ffcfa07106f0795ddccd6ece2d4e
pimarchiol <152488@spes.uniud.it>
2025-02-21 15:33:59 +0100

    Added one variable for the Pub/Sub topic of each function

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "project_id" {
   description = "The GCP project ID of the serverless project 24W"
   type = string
+  default = "serverless-project-24w"
 }
 
 variable "region" {
@@ -16,9 +17,9 @@ variable "function_memory" {
     "input_handler" = "256MB"
     "metadata_extractor" = "512MB"
     "exif_processor" = "256MB"
-    "format_converter" = "512MB"
+    "format_converter" = "1024B"
     "thumbnail_generator" = "512MB"
-    "rgb_channel_separator" = "512MB"
+    "rgb_channel_separator" = "1024MB"
     "metrics_visualizer" = "256MB"
   }
 }
@@ -36,3 +37,23 @@ variable "function_timeout" {
     "metrics_visualizer"  = 120
   }
 }
+
+variable "pubsub_topics"{
+  description = "Pub/Sub topics for triggering functions"
+  type = map{string}
+  default = {
+    "input_handler" = "input-handler-topic"
+    "metadata_extractor" = "metadata-extractor-topic"
+    "exif_processor" = "exif-processor-topic"
+    "format_converter" = format-converter-topic
+    "thumbnail_generator" = "thumbnail-generator-topic"
+    "rgb_channel_separator" = "rgb-channel-separator-topic"
+    "metrics_visualizer" = "metrics-visualizer-topic"
+  }
+}
+
+
+
+
+
+
